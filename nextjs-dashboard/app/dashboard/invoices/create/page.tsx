46246c872092d0d705451078cbb50fb678e6adbb
niksadoman <mdnazari@lancaster.ac.uk>
2026-04-15 10:21:58 +0800

    Completed Chapter 10

---
+++ nextjs-dashboard/app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,15 @@
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+
+export default async function Page() {
+  const customers = await fetchCustomers();
+    return (
+    <>
+      <Breadcrumbs
+        breadcrumbs={[{ label: 'Invoices', href: '/dashboard/invoices' }]}
+      />
+      <Form customers={customers} />
+    </>
+  );
+}
